7a4e77ba673aac5e177b3e426e5a2663e8f87276
valestanov <socredelir@163.com>
2021-12-14 20:59:31 +0100

    Fix bugs in setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="PolyglotLinguisticToolkit",
@@ -7,7 +7,7 @@ setup(
     author_email="",
     description="Polyglot Multilingual Linguistic Toolkit. Transliteration and process of many language, especially in Asia.",
     url="https://github.com/valestanov/LinguisticToolkit",
-    packages=find_packages()
+    packages=find_packages(),
     license='MIT License',
 )
 
