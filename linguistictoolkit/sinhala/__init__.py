fb59c2b59f81caaad62f6ed84a60262cb21a85f4
valestanov <socredelir@163.com>
2021-12-14 23:17:55 +0100

    Some fix

--- linguistictoolkit/sinhala/__init__.py
+++ linguistictoolkit/sinhala/__init__.py
@@ -3,5 +3,5 @@
 Written in 20-22 March 2016 in the reading area of the 1 stock of the Library of Jilin University.
 '''
 
-from romanization import transliterate
-from abhayaConverter import abhaya_unicode_converter
+from linguistictoolkit.sinhala.romanization import transliterate
+from linguistictoolkit.sinhala.abhayaConverter import abhaya_unicode_converter
