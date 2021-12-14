4de05dfee454abb4cf679e61860367c2c6b113a3
valestanov <socredelir@163.com>
2021-12-14 20:35:04 +0100

    Setup file and Sinhala romanisation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PolyglotLinguisticToolkit",
+    version="0.1",
+    author="H-W. Liu (V.A.Lestanov)",
+    author_email="",
+    description="Polyglot Multilingual Linguistic Toolkit. Transliteration and process of many language, especially in Asia.",
+    url="https://github.com/valestanov/LinguisticToolkit",
+    license='MIT License',
+    python_requires='>=3',
+)
+
