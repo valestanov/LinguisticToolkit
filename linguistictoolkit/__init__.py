f822339ac27730bb0b59a3c126d8349e3a05ef8e
valestanov <socredelir@163.com>
2024-03-30 00:41:56 +0100

    Update __init__.py

--- linguistictoolkit/__init__.py
+++ linguistictoolkit/__init__.py
@@ -5,5 +5,6 @@ from linguistictoolkit.tools import yubao
 from linguistictoolkit.tools import rime
 from linguistictoolkit.tools import ankiproc
 from linguistictoolkit.tools import aegisub
+from linguistictoolkit.tools import widget
 from linguistictoolkit.korean import ankimasker
 from linguistictoolkit.vietnamese import Qnc2Telex
