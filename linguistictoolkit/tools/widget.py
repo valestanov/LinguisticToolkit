aa7df6cabeab8f09c487cc917b3c5c42e768569f
valestanov <socredelir@163.com>
2024-05-11 11:42:35 +0200

    Widget - complete func textsplit

--- linguistictoolkit/tools/widget.py
+++ linguistictoolkit/tools/widget.py
@@ -1,9 +1,23 @@
 enabc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 enabc = enabc + enabc.lower()
+iscn = lambda x:x>='一'
 
 def langsplit(line,islang):
-    linelist = [islang(char) for char in line]
+    linelist = [islang(char) or char=='\t' for char in line]
     if True in linelist:
-        return line[:linelist.index(True)],line[linelist.index(True):]
+        pos = linelist.index(True)
+        left = line[:pos].strip()
+        right = line[pos:].replace('\t','').strip()
+        while '  ' in left:
+            left = left.replace('  ',' ')
+        while '  ' in right:
+            right = right.replace('  ',' ')
+        return left,right
     else:
         return line,''
+
+def textsplit(text,islang):
+    text = text.replace('\ufeff','').replace('\r','').split('\n')
+    textsplit = [langsplit(line,islang) for line in text]
+    textsplit = ['\t'.join(line) for line in text]
+    return '\n'.join(textsplit)
