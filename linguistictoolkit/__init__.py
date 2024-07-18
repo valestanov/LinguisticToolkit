db0251bc3936a668dbb2f415ebfa868df78064fd
valestanov <socredelir@163.com>
2024-07-18 19:58:12 +0200

    Update __init__.py

--- linguistictoolkit/__init__.py
+++ linguistictoolkit/__init__.py
@@ -6,5 +6,8 @@ from linguistictoolkit.tools import rime
 from linguistictoolkit.tools import ankiproc
 from linguistictoolkit.tools import aegisub
 from linguistictoolkit.tools import widget
+from linguistictoolkit.tools import interface
+from linguistictoolkit.tools import langdeterminer
+from linguistictoolkit.tools import engines
 from linguistictoolkit.korean import ankimasker
 from linguistictoolkit.vietnamese import Qnc2Telex
